f6a96e77932dbf274002e01bc1abad848d5a9007
Christopher Zöllner <chriszoelln@gmail.com>
2023-07-12 21:34:10 +0200

    createUser

--- handler.go
+++ handler.go
@@ -12,3 +12,14 @@ var posts []Post
 func getUsers(c*gin.Context){
 	c.IndentedJSON(http.StatusOK, users)
 }
+
+func createUser(c*gin.Context){
+	var user User
+	if err := c.BindJSON(&user); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	users = append(users, user)
+	c.JSON(http.StatusCreated, user)
+}
--- routes.go
+++ routes.go
@@ -5,4 +5,5 @@ import "github.com/gin-gonic/gin"
 func InitializeRoutes(router*gin.Engine){
 	// user routes
 	router.GET("/users", getUsers)
+	router.POST("/users", createUser)
 }
